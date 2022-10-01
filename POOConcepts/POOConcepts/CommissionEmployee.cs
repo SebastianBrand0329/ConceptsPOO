f0291848b2d80e537938faafcf1fdbe30686643d
SEBASTIAN BRAND TAMAYO <sebastianbrand232390@correo.itm.edu.co>
2022-10-01 10:15:12 -0500

    Employee, SalaryEmployee & CommissionEmployee Finish

---
+++ POOConcepts/POOConcepts/CommissionEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    internal class CommissionEmployee : Employee
+    {
+        public float CommssionPercentaje { get; set; }
+        public decimal Sales { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)CommssionPercentaje;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission percentaje: {$"{CommssionPercentaje:P2}", 15}" +
+                $"\n\tSales................: {$"{Sales:C2}", 15}" +
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}", 15}";
+        }
+    }
+}
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -3,14 +3,29 @@ using ConceptsPOO;
 
 Console.WriteLine("POO Conpcepts");
 Console.WriteLine("==============");
-try
+Employee employee1 = new SalaryEmployee()
 {
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(1974, 9, 23));
-	Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
-{
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45m
+};
 
-	Console.WriteLine(error.Message);
-}
+Console.WriteLine(employee1);
+
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    CommssionPercentaje = 0.03F,
+    Sales = 65000000
+};
+Console.WriteLine(employee2);
