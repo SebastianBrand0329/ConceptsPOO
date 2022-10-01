60025d601bd7aab20e8962580c1e70b18274cd8b
SEBASTIAN BRAND TAMAYO <sebastianbrand232390@correo.itm.edu.co>
2022-10-01 08:59:57 -0500

    Finish Date class

---
+++ POOConcepts/POOConcepts/DateException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class DateException : Exception
+    {
+        public DateException()
+        {
+        }
+
+        public DateException(string message) : base(message)
+        {
+        }
+
+        public DateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DateException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
